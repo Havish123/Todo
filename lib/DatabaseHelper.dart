d88b4d4aaa15823d8370eafd98c63723c768349a
Havish123 <havishnadar@gmail.com>
2021-04-21 09:45:15 +0530

    final

--- lib/DatabaseHelper.dart
+++ lib/DatabaseHelper.dart
@@ -1,65 +1,59 @@
-
-import 'dart:io' ;
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
-class DataBaseHelper{
+class DataBaseHelper {
   DataBaseHelper._();
-  static final DataBaseHelper instance=DataBaseHelper._();
+  static final DataBaseHelper instance = DataBaseHelper._();
   static Database _database;
-  
-  static final table_name="todo_list";
-  static final coloum="list";
-  static final id="col_id";
-  static final db_name="todo.db";
-  
-  Future<Database> get database async{
-    if(_database !=null){
+
+  static final table_name = "todo_list";
+  static final coloum = "list";
+  static final id = "col_id";
+
+  static final db_name = "todo.db";
+
+  Future<Database> get database async {
+    if (_database != null) {
       return _database;
     }
-    _database =await initDB();
+    _database = await initDB();
     return _database;
   }
-  
-  initDB() async{
-    Directory directory=await getApplicationDocumentsDirectory();
-    String path = join(directory.path,db_name);
-    return await openDatabase(
-        path,
-      version: 1,
-      onCreate: (Database db,int version) async{
-          await db.execute(
-            '''
+
+  initDB() async {
+    Directory directory = await getApplicationDocumentsDirectory();
+    String path = join(directory.path, db_name);
+    return await openDatabase(path, version: 1,
+        onCreate: (Database db, int version) async {
+      await db.execute('''
             CREATE TABLE $table_name(
             $id INTEGER PRIMARY KEY,$coloum TEXT NOT NULL);
             
-            '''
-          );
-      }
-
-    );
-    
+            ''');
+    });
   }
 
-  Future<int> insert(Map<String,dynamic> row) async{
-    Database db=await instance.database;
+  Future<int> insert(Map<String, dynamic> row) async {
+    Database db = await instance.database;
     return await db.insert(table_name, row);
   }
 
-  Future<List<Map<String,dynamic>>> viewquery() async{
-    Database db=await instance.database;
+  Future<List<Map<String, dynamic>>> viewquery() async {
+    Database db = await instance.database;
     return await db.query(table_name);
   }
 
-  Future<int> update(Map<String,dynamic> row) async{
-    Database db=await instance.database;
-    return await db.update(table_name, row,where: '$id=?',whereArgs: [id]);
-}
- Future<int> delete(int id) async{
-    Database db=await instance.database;
-    return await db.delete(table_name,where:'$id=?', whereArgs:[id] );
- }
+  Future<int> update(Map<String, dynamic> row) async {
+    Database db = await instance.database;
+    return await db.update(table_name, row, where: '$id=?', whereArgs: [id]);
+  }
+
+  Future<int> delete(int id) async {
+    Database db = await instance.database;
+    return await db.delete(table_name, where: '$id=?', whereArgs: [id]);
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'DatabaseHelper.dart';
@@ -5,106 +7,119 @@ import 'DatabaseHelper.dart';
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title:"TO_DO Application",
+      title: "TO_DO Application",
       home: todo(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
+
 class todo extends StatefulWidget {
   @override
   _todoState createState() => _todoState();
 }
 
-class Todo{
+class Todo {
   int id;
   String task;
+  bool boolean;
 
-  Todo({this.id,this.task});
+  Todo({this.id, this.task, this.boolean});
 
-  Map<String,dynamic> toMap(){
+  Map<String, dynamic> toMap() {
     return {
-      'id':id,'task':task,
+      'id': id,
+      'task': task,
+      'boolean': boolean,
     };
   }
 }
-class _todoState extends State<todo> {
 
-  List<Todo> tasklist=new List();
+class _todoState extends State<todo> {
+  List<Todo> tasklist = new List();
+
+  // void navigateSecondPage() {
+  //   Route route = MaterialPageRoute(builder: (context) => todo());
+  //   Navigator.push(context, route).then(onGoBack);
+  // }
 
   @override
   void initState() {
-
     super.initState();
 
-    DataBaseHelper.instance.viewquery().then((value){
+    DataBaseHelper.instance.viewquery().then((value) {
       setState(() {
-        value.forEach((element){
-          tasklist.add(Todo(id:element['col_id'],task:element['list']));
+        value.forEach((element) {
+          tasklist.add(Todo(
+            id: element['col_id'],
+            task: element['list'],
+            boolean: true,
+          ));
         });
       });
     });
-
   }
 
   Future<void> _displayTextInputDialog(BuildContext context) async {
-    TextEditingController _textFieldController=new TextEditingController();
+    TextEditingController _textFieldController = new TextEditingController();
     return showDialog(
         context: context,
         builder: (context) {
           return AlertDialog(
-            title: Text('TextField in Dialog'),
+            title: Text('Add Todos....'),
             content: TextField(
-              onChanged: (value) {
-
-              },
+              onChanged: (value) {},
               controller: _textFieldController,
-              decoration: InputDecoration(
-                  hintText: "Text Field in Dialog"),
+              decoration: InputDecoration(hintText: "Enter the items..."),
             ),
             actions: [
               new Row(
-
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-
                   Container(
-                    margin:EdgeInsets.only(left:5.0,right: 20.0),
-                    child:CupertinoButton(
-
+                    margin: EdgeInsets.only(left: 5.0, right: 20.0),
+                    child: CupertinoButton(
                       color: Colors.blue,
                       child: Text('Cancel'),
-                      onPressed: (){
+                      onPressed: () {
                         setState(() {
                           Navigator.pop(context);
                         });
                       },
-
                       padding: EdgeInsets.all(10),
                     ),
                   ),
                   Container(
-
-                    margin:EdgeInsets.only(right:5.0,left: 30.0),
-                    child:CupertinoButton(
-
+                    margin: EdgeInsets.only(right: 5.0, left: 30.0),
+                    child: CupertinoButton(
                         color: Colors.blue,
                         child: Text('Add Items'),
-                        onPressed: (){
-
+                        onPressed: () {
                           setState(() async {
-                            var text=_textFieldController.text;
-                            var id=await DataBaseHelper.instance.insert({DataBaseHelper.coloum : text});
+                            var text = _textFieldController.text;
+                            var id = await DataBaseHelper.instance
+                                .insert({DataBaseHelper.coloum: text});
                             setState(() {
-                              tasklist.insert(0, Todo(id:id,task: text));
+                              tasklist.insert(
+                                  0, Todo(id: id, task: text, boolean: true));
+                              Navigator.pop(
+                                context,
+                              );
                             });
-                            Navigator.pop(context);
+
+                            // Navigator.pop(
+                            //     context,
+                            //     MaterialPageRoute(
+                            //         builder: (BuildContext context) =>
+                            //             new todo()));
                           });
-                        },padding: EdgeInsets.all(10)
-                    ),
+                        },
+                        padding: EdgeInsets.all(10)),
                   ),
                 ],
               )
@@ -115,43 +130,75 @@ class _todoState extends State<todo> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text('TO-DO Application'),
-        actions: <Widget> [
-          IconButton(icon:Icon(Icons.add),onPressed: () {
-            _displayTextInputDialog(context);
-          },
-      ),],
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              _displayTextInputDialog(context);
+            },
+          ),
+        ],
       ),
       body: ListView.builder(
-        itemCount: tasklist.length,
-        itemBuilder: (context,index){
-          return ListTile(
-            title:Text(tasklist[index].task),
+          itemCount: tasklist.length,
+          itemBuilder: (context, index) {
+            return buildListTile(index);
+          }),
+    );
+  }
 
-            leading: Text(tasklist[index].id.toString()),
-            trailing: CupertinoButton(child: Text('Done'),padding: EdgeInsets.only(left:30,right: 30),
-            onPressed: (){
-              _deleteTask(tasklist[index].id);
-            },),
+  changeStyle(int index) {
+    setState(() {
+      tasklist[index].boolean = false;
+    });
+  }
 
+  bool styleOBJ = true;
 
+  ListTile buildListTile(int index) {
+    return ListTile(
+      title: Padding(
+        padding: EdgeInsets.all(10),
+        child: buildText(index),
+      ),
 
-          );
-        }
+      // leading: Text(tasklist[index].id.toString()),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          CupertinoButton(
+            child: Text('Done'),
+            // padding: EdgeInsets.only(left: 30, right: 30),
+            onPressed: () {
+              changeStyle(index);
+            },
+          ),
+          IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {
+                _deleteTask(tasklist[index].id);
+              }),
+        ],
       ),
     );
   }
 
-  void _deleteTask(int id) async{
-    await DataBaseHelper.instance.delete(id);
-    setState(() {
-      tasklist.removeWhere((element) => element.id==id);
-    });
+  Text buildText(int index) {
+    return Text(
+      tasklist[index].task,
+      style: tasklist[index].boolean
+          ? TextStyle(decoration: TextDecoration.none, fontSize: 20)
+          : TextStyle(decoration: TextDecoration.lineThrough, fontSize: 20),
+    );
   }
 
-
+  void _deleteTask(int id) async {
+    await DataBaseHelper.instance.delete(id);
+    setState(() {
+      tasklist.removeWhere((element) => element.id == id);
+    });
+  }
 }
-
